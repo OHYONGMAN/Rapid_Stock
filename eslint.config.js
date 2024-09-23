c78f393237bf6be444c9f27ae84367e47fe0ab78
OHYONGMAN <oymin1998@naver.com>
2024-09-23 17:39:37 +0900

    프로젝트 세팅

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettierPluginRecommended from 'eslint-plugin-prettier/recommended';
+import tailwindPlugin from 'eslint-plugin-tailwindcss';
+
+export default tseslint.config(
+  // ...tseslint.configs.recommendedTypeChecked,
+  // ...tseslint.configs.stylisticTypeChecked,
+  ...tailwindPlugin.configs['flat/recommended'],
+  prettierPluginRecommended,
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'warn',
+    },
+  },
+);
