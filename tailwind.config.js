2024d769df235292ac869ee0d0f5548868f51d9c
진승현 <hynn@jinseunghyeon-ui-MacBookPro.local>
2024-09-25 09:57:05 +0900

    style : tailwind 전역 스타일 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#e60000',
+        secondary: '#ff7308',
+        'g-100': 'F8F9FA',
+        'g-200': 'F4F4F6',
+        'g-300': 'EEF0F2',
+        'g-400': 'E0E3E6',
+        'g-500': 'C2C6CA',
+        'g-600': '9FA3A7',
+        ' g-700': '7B8184',
+        ' g-800': '63666A',
+        ' g-900': '4E5154',
+      },
+    },
   },
   plugins: [],
 };
