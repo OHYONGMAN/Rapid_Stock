f079d67d8ea0ba9f5951d0d7381ff4d99b180190
진승현 <hynn@jinseunghyeon-ui-MacBookPro.local>
2024-09-25 15:30:31 +0900

    docs : Footer 작업

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 import NewsPage from './components/main/newsPage/NewsPage';
-import Community from './components/main/community/Community';
-import Login from './components/main/login/Login';
-import SignUp from './components/main/signUp/SignUp';
 
 const App: React.FC = () => {
-  // const [count, setCount] = useState(0);
-
   const links = [
     { name: '뉴스룸', url: '/news' },
     { name: '커뮤니티', url: '/community' },
     { name: '모의투자', url: '/stock' },
-
   ];
 
   return (
-    <Router>
-      <Header links={links} />
-      <Routes>
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Header links={links} />
+        <Routes>
+          <Route path="/news" element={<NewsPage />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 };
 
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from 'react';
+import logo from '/src/assets/img/logo-gray.svg';
 
-export default function Footer() {
+const Footer: React.FC = () => {
+  const infoLinks = [
+    { name: '오용민', url: 'https://github.com/ynn-i' },
+    { name: '장나라', url: 'https://github.com/ynn-i' },
+    { name: '장유주', url: 'https://github.com/ynn-i' },
+    { name: '지명진', url: 'https://github.com/ynn-i' },
+    { name: '진승현', url: 'https://github.com/ynn-i' },
+  ];
+
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-full fixed bottom-0 left-0 bg-g-100 text-g-600">
+      <div className="container mx-auto flex flex-col justify-between py-12">
+        <div className="flex items-start gap-20">
+          <img src={logo} alt="rapid stock" className="w-60" />
+          <div>
+            <ul className="list-none grid grid-cols-3 gap-x-12">
+              {infoLinks.map((infoLink, index) => (
+                <li key={index}>
+                  <a
+                    href={infoLink.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline text-g-600"
+                  >
+                    {infoLink.name} | {infoLink.url}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <p className="mt-4 text-sm">
+              &copy; 2024 Rapid Stock. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
